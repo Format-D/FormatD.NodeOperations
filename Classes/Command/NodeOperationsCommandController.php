951bbede6e2d8550a17e78e51234712e9f7b918f
Benno Weinzierl <bj@format-d.com>
2018-09-13 13:29:45 +0200

    [INIT] Initial commit

---
+++ Classes/Command/NodeOperationsCommandController.php
@@ -0,0 +1,121 @@
+<?php
+namespace FormatD\NodeOperations\Command;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Cli\CommandController;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class NodeOperationsCommandController extends CommandController {
+
+	/**
+	 * @Flow\Inject
+	 * @var \Neos\Flow\Utility\Now
+	 */
+	protected $now;
+
+	/**
+	 * @Flow\Inject
+	 * @var \Neos\Neos\Service\NodeOperations
+	 */
+	protected $nodeOperationService;
+
+	/**
+	 * @Flow\Inject
+	 * @var \Neos\ContentRepository\Domain\Service\ContextFactoryInterface
+	 */
+	protected $contextFactory;
+
+	/**
+	 * Move node with $nodeIdentifier before, into or after targetNode with $targetNodeIdentifier
+	 *
+	 * @param string $nodeIdentifier
+	 * @param string $targetNodeIdentifier
+	 * @param string $position where the node should be added (allowed: before, into, after)
+	 * @return void
+	 */
+	public function moveCommand($nodeIdentifier, $targetNodeIdentifier, $position)
+	{
+		if (!in_array($position, ['before', 'into', 'after'], TRUE)) {
+			$this->outputLine('Unrecognised position: "' . $position . '" (possible options: before, into, after)');
+			return;
+		}
+
+		$nodeContext = $this->getContext();
+
+		$node = $nodeContext->getNodeByIdentifier($nodeIdentifier);
+		if (!$node) {
+			$this->outputLine('Node with identifier "' . $nodeIdentifier . '" not found!');
+			return;
+		}
+
+		$targetNode = $nodeContext->getNodeByIdentifier($targetNodeIdentifier);
+		if (!$targetNode) {
+			$this->outputLine('TargetNode with identifier "' . $targetNodeIdentifier . '" not found!');
+			return;
+		}
+
+		$this->outputLine('Moving node "' . $node->getLabel() . '" ' . $position . ' node "' . $targetNode->getLabel() . '"...');
+
+		$this->nodeOperationService->move($node, $targetNode, $position);
+
+		$this->outputLine('Done!');
+	}
+
+	/**
+	 * Copy node with $nodeIdentifier before, into or after targetNode with $targetNodeIdentifier
+	 *
+	 * @param string $nodeIdentifier
+	 * @param string $targetNodeIdentifier
+	 * @param string $position where the new node should be created (allowed: before, into, after)
+	 * @param string $name of the new node
+	 * @return void
+	 */
+	public function copyCommand($nodeIdentifier, $targetNodeIdentifier, $position, $name = NULL)
+	{
+		if (!in_array($position, ['before', 'into', 'after'], TRUE)) {
+			$this->outputLine('Unrecognised position: "' . $position . '" (possible options: before, into, after)');
+			return;
+		}
+
+		$nodeContext = $this->getContext();
+
+		$node = $nodeContext->getNodeByIdentifier($nodeIdentifier);
+		if (!$node) {
+			$this->outputLine('Node with identifier "' . $nodeIdentifier . '" not found!');
+			return;
+		}
+
+		$targetNode = $nodeContext->getNodeByIdentifier($targetNodeIdentifier);
+		if (!$targetNode) {
+			$this->outputLine('TargetNode with identifier "' . $targetNodeIdentifier . '" not found!');
+			return;
+		}
+
+		$this->outputLine('Copying node "' . $node->getLabel() . '" ' . $position . ' node "' . $targetNode->getLabel() . '"...');
+
+		$this->nodeOperationService->copy($node, $targetNode, $position, $name);
+
+		$this->outputLine('Done!');
+	}
+
+	/**
+	 * @return \Neos\ContentRepository\Domain\Service\Context
+	 */
+	protected function getContext()
+	{
+		if (method_exists($this->now, '_activateDependency')) {
+			$this->now->_activateDependency();
+		}
+
+		return $this->contextFactory->create(array(
+			'workspaceName' => 'live',
+			'currentDateTime' => $this->now,
+			'invisibleContentShown' => FALSE,
+			'removedContentShown' => FALSE,
+			'inaccessibleContentShown' => FALSE,
+			'dimensions' => array(),
+		));
+	}
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+
+# FormatD.NodeOperations
+
+CLI commands for moving and copying nodes.
+This is useful if you want to move nodes with a lot of childnodes. This can take a long while and make the UI crash.
+
+## Move nodes
+
+Example:
+```
+    ./flow nodeoperations:move 1fd7d006-d1db-4s5c-ac89-7170fe22ce24 a77eaf64-eed9-4deb-9d21-52325d04d761 into
+```
+## Copy nodes
+
+```
+    ./flow nodeoperations:copy 1fd7d006-d1db-4s5c-ac89-7170fe22ce24 a77eaf64-eed9-4deb-9d21-52325d04d761 after 'New Name'
+```
